43ebb4c532384c7217f741620e0b12679304cd1d
Arya Putra <ariaryaputra5@gmail.com>
2025-11-02 22:21:09 +0700

    adding route and automatic buyer role after register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('admin')->name('admin.')->group(function(){
+            Route::resource('products', ProductController::class)->middleware('role:owner');
+            Route::resource('categories', CategoryController::class)->middleware('role:owner');
+    });
 });
 
 require __DIR__.'/auth.php';
